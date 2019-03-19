59cdeb7a5c51336ef746919371d2233092b1a98f
Levonka <levonkana@gmail.com>
2019-03-19 17:33:44 +0300

    add functions which make ss instruction and remove rra and ra

--- push_swap_prog/edit_instructions.c
+++ push_swap_prog/edit_instructions.c
@@ -1,19 +1,53 @@
 #include "../inc/push_swap.h"
 
+void	hlop(t_st *st, int len, int i)
+{
+	int		j;
+	int		k;
+
+	j = 0;
+	while (j < len)
+	{
+		k = i;
+		while (k < st->instr_end)
+		{
+			st->instr[k] = st->instr[k + 1];
+			k++;
+		}
+		j++;
+	}
+	st->instr_end -= len;
+	st->instr[st->instr_end] = '\0';
+}
+
+void	sasb(t_st *st, int *i)
+{
+	if (st->instr[*i] == '1' && st->instr[*i + 1] == '2')
+	{
+		hlop(st, 1, *i);
+		st->instr[*i] = '3';
+		*i -= 5;
+	}
+}
+
+void	rrara(t_st *st, int *i)
+{
+	if (st->instr[*i] == '9' && st->instr[*i + 1] == '6')
+	{
+		hlop(st, 2, *i);
+		*i -= 5;
+	}
+}
+
 void	edit_instructions(t_st *st)
 {
 	int		i;
-	int		size;
 
 	i = 0;
-	size = ft_strlen(st->instr);
-	while (i + 1 < size)
+	while (i + 1 < st->instr_end)
 	{
-		if (st->instr[i] == '1' && st->instr[i + 1] == '2')
-		{
-			st->instr[i] = '3';
-			st->instr[i + 1] = 'c';
-		}
+		sasb(st, &i);
+		rrara(st, &i);
 		i++;
 	}
 }
